0661eade43add0a53d44db1c23044f771b06346d
MaleneRJohansen <malene9@hotmail.no>
2024-04-05 12:35:42 +0200

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const BookCard = ({book, amazonURL}) => {
+
   return (
     <>
-    <div>
+    <div className='bookCard'>
         <h2>{book.title}</h2>
         <p>Forfatter: {book.author_name && book.author_name.join(', ')}</p>
         <p>Rating: {book.ratings_average}</p>
