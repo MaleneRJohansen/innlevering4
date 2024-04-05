3d1b594150bde535ac4507d5d9765394aee34cf7
MaleneRJohansen <malene9@hotmail.no>
2024-04-05 12:16:54 +0200

    Korrigert skrivefeil

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -8,7 +8,7 @@ const BookCard = ({book, amazonURL}) => {
         <p>Forfatter: {book.author_name && book.author_name.join(', ')}</p>
         <p>Rating: {book.ratings_average}</p>
         <p>Publiseringsår: {book.first_publish_year}</p>
-        <a href={amazonURL(book.amazon_id)} target='_blank' rel='noopener noreferrer'>Kjøp på Amazon</a>
+        <a href={amazonURL(book.id_amazon)} target='_blank' rel='noopener noreferrer'>Kjøp på Amazon</a>
     </div>
     </>
   )
